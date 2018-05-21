d364c505867cb0543609b55419016975459a1d44
Jan Dolanski <mrjdolanski@gmail.com>
2018-05-21 14:57:47 +0200

    init

---
+++ template-1.groovy
@@ -0,0 +1,14 @@
+xlr {
+  release('template001') {
+    phases {
+      phase('Phase1') {
+        color '#0099CC'
+        tasks {
+          manual('a') {
+            
+          }
+        }
+      }
+    }
+  }
+}
