d7d697fd652c2a321578b47f63a5dffb17ef48b5
jdolanski <jdolanski@users.noreply.github.com>
2018-05-29 14:47:58 +0200

    vv444

--- temp2dir/temp2.groovy
+++ temp2dir/temp2.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Jans-MBP:5516/#/templates/Folder8491c205ca70438993ae8a2a6f07bceb-Releaseddb3ca65ffb5441eb16ff26df615f7ef/releasefile
 // XL Release version:   0.0.0-SNAPSHOT
-// Date created:         Tue May 29 14:47:10 CEST 2018
+// Date created:         Tue May 29 14:47:57 CEST 2018
 
 def scmConnectorConfig1 = 'PLEASE IMPLEMENT THIS METHOD'
 
@@ -13,11 +13,7 @@ xlr {
       phase('New Phase') {
         tasks {
           manual('a') {
-            attachments {
-              attachment("sql:Attachmente8fe10b8ce434908acc32c15bcc9dd7f") {
-                filename "attachments/Attachmente8fe10b8ce434908acc32c15bcc9dd7f-image.png"
-              }
-            }
+            
           }
           manual('b') {
             
