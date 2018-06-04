239fb43c2c0e56a582cf59e482e2cf7fbc87eb74
jdolanski <jdolanski@users.noreply.github.com>
2018-06-04 10:12:15 +0200

    v3.4.3.3

---
+++ new_temp.groovy
@@ -0,0 +1,45 @@
+// Exported from:        http://Jans-MBP:5516/#/templates/Release60214d2073f44ff689aec2cb10b46d08/releasefile
+// XL Release version:   0.0.0-SNAPSHOT
+// Date created:         Mon Jun 04 10:12:14 CEST 2018
+
+def scmConnectorConfig1 = 'PLEASE IMPLEMENT THIS METHOD'
+
+xlr {
+  template('newTemp') {
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-06-04T09:00:00+0200')
+    scmConnectorConfig scmConnectorConfig1
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('afda') {
+            
+          }
+          manual('fda') {
+            
+          }
+          manual('f') {
+            
+          }
+          manual('fd') {
+            
+          }
+          manual('a') {
+            
+          }
+          manual('f') {
+            
+          }
+        }
+      }
+    }
+    teams {
+      team('Template Owner') {
+        members 'admin'
+        permissions 'template#edit', 'template#lock_task', 'template#view', 'template#edit_triggers', 'template#edit_security', 'template#create_release'
+      }
+      team('Release Admin') {
+        permissions 'release#edit', 'release#lock_task', 'release#start', 'release#reassign_task', 'release#edit_blackout', 'template#view', 'release#edit_security', 'release#abort', 'release#view', 'release#edit_task'
+      }
+    }
+  }
+}
