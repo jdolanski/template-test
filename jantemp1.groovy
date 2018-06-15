c1f0026b90742803600c19b6a57d26aefb163436
jdolanski <jdolanski@users.noreply.github.com>
2018-06-15 11:57:53 +0200

    fdas

---
+++ jantemp1.groovy
@@ -0,0 +1,26 @@
+// Exported from:        http://Jans-MBP:5516/#/templates/Folderfca5a290e9f4c2f8fb1a74c15e9dadb-Release130663c0ba7485c9ea30060a7bf068e/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Fri Jun 15 11:57:51 CEST 2018
+
+xlr {
+  template('template') {
+    folder('folder1')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-06-15T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('a') {
+            
+          }
+          manual('b') {
+            
+          }
+          manual('c') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
