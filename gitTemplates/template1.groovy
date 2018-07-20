afff8233c676561de44ceba56f9179ce05ef13af
jdolanski <jdolanski@users.noreply.github.com>
2018-07-20 14:11:41 +0200

    k

--- gitTemplates/template1.groovy
+++ gitTemplates/template1.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Jans-MacBook-Pro.local:5516/#/templates/Folder3b39231f69db411f9ad5e63577d8c01b-Releasea65b7e8131543318bc97da8ef6474d1/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Fri Jul 20 14:09:25 CEST 2018
+// Date created:         Fri Jul 20 14:11:07 CEST 2018
 
 xlr {
   template('githubTemplate') {
