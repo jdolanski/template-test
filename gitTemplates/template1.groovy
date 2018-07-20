ca51fb736ae9594680203ec4f49dd05fce6cf889
jdolanski <jdolanski@users.noreply.github.com>
2018-07-20 14:09:58 +0200

    a

---
+++ gitTemplates/template1.groovy
@@ -0,0 +1,23 @@
+// Exported from:        http://Jans-MacBook-Pro.local:5516/#/templates/Folder3b39231f69db411f9ad5e63577d8c01b-Releasea65b7e8131543318bc97da8ef6474d1/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Fri Jul 20 14:09:25 CEST 2018
+
+xlr {
+  template('githubTemplate') {
+    folder('scmtemplates')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-07-20T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('a') {
+            
+          }
+          manual('b') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
