fb4bd3a7127c6860963a008db392c3a38cba1bf1
jdolanski <jdolanski@users.noreply.github.com>
2018-07-27 14:06:58 +0200

    fdafa

---
+++ test.groovy
@@ -0,0 +1,35 @@
+// Exported from:        http://Jans-MBP:5516/#/templates/Folder560dc180ea034408aa5d3ad2b7773119-Releasef74b2732158e452b9d7cae7f6edd56aa/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Fri Jul 27 14:06:57 CEST 2018
+
+xlr {
+  template('template1') {
+    folder('f1')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-07-27T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('g') {
+            
+          }
+          manual('fgd') {
+            
+          }
+          manual('fdg') {
+            
+          }
+          manual('f') {
+            
+          }
+          manual('gd') {
+            
+          }
+          manual('fgd') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
