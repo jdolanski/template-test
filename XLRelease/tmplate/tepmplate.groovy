54e7f6076841ad67f2eb0abb5a2b13621ed3a49b
jdolanski <jdolanski@users.noreply.github.com>
2018-08-02 10:50:57 +0200

    fsafdafaf

---
+++ XLRelease/tmplate/tepmplate.groovy
@@ -0,0 +1,35 @@
+// Exported from:        http://Jans-MBP:5516/#/templates/Folder5e77afbd37e54f478c4c8f5c5f55314a-Release8afa6b56d03248939e57fb4bd579307c/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Thu Aug 02 10:50:55 CEST 2018
+
+xlr {
+  template('tepmplate') {
+    folder('tmplate')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-02T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('a') {
+            
+          }
+          manual('vv') {
+            
+          }
+          manual('s') {
+            
+          }
+          manual('f') {
+            
+          }
+          manual('sd') {
+            
+          }
+          manual('sf') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
