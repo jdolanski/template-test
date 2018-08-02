4d81411109e5e524f80f527128dded87da5071c0
jdolanski <jdolanski@users.noreply.github.com>
2018-08-02 12:15:58 +0200

    dfafa

--- XLRelease/tmplate/tepmplate.groovy
+++ XLRelease/tmplate/tepmplate.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Jans-MBP:5516/#/templates/Folder5e77afbd37e54f478c4c8f5c5f55314a-Release8afa6b56d03248939e57fb4bd579307c/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Thu Aug 02 11:56:41 CEST 2018
+// Date created:         Thu Aug 02 12:15:56 CEST 2018
 
 xlr {
   template('tepmplate') {
@@ -9,15 +9,6 @@ xlr {
     phases {
       phase('New Phase') {
         tasks {
-          manual('a') {
-            
-          }
-          manual('vv') {
-            
-          }
-          manual('s') {
-            
-          }
           manual('f') {
             
           }
