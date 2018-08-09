f0489d9ac92460e52f3adefabbc0cfa299b29545
jdolanski <jdolanski@users.noreply.github.com>
2018-08-09 13:17:46 +0200

    sklkldfdklfkdsldsklklsd

---
+++ XLRelease/a/submfolder/templateinsumbflolder.groovy
@@ -0,0 +1,29 @@
+// Exported from:        http://Jans-MBP:5516/#/templates/Folder837d6329e8c44393af3207c84c9b4fff-Folder81808dba02934e72aa9e6c40814c3e55-Release63cc2e18d29f4ccd86bfcf19b116af2f/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Thu Aug 09 13:17:45 CEST 2018
+
+xlr {
+  template('templateinsumbflolder') {
+    folder('a/submfolder')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-09T09:00:00+0200')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('fafa') {
+            
+          }
+          manual('f') {
+            
+          }
+          manual('fd') {
+            
+          }
+          manual('af') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
