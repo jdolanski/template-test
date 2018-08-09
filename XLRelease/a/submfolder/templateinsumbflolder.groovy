061d86f0f8b8c95cb8456b57792e2785c92dfbdd
jdolanski <jdolanski@users.noreply.github.com>
2018-08-09 13:19:21 +0200

    fllsdflljkdljkdsfkllkjdskldfs

--- XLRelease/a/submfolder/templateinsumbflolder.groovy
+++ XLRelease/a/submfolder/templateinsumbflolder.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Jans-MBP:5516/#/templates/Folder837d6329e8c44393af3207c84c9b4fff-Folder81808dba02934e72aa9e6c40814c3e55-Release63cc2e18d29f4ccd86bfcf19b116af2f/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Thu Aug 09 13:17:45 CEST 2018
+// Date created:         Thu Aug 09 13:19:20 CEST 2018
 
 xlr {
   template('templateinsumbflolder') {
@@ -12,13 +12,13 @@ xlr {
           manual('fafa') {
             
           }
-          manual('f') {
+          manual('af') {
             
           }
           manual('fd') {
             
           }
-          manual('af') {
+          manual('f') {
             
           }
         }
