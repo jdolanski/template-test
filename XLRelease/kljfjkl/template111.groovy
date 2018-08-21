8eeb37ccaae3a6235a08135c7722da75fd92e680
jdolanski <jdolanski@users.noreply.github.com>
2018-08-21 14:13:42 +0200

    sdfasfa

---
+++ XLRelease/kljfjkl/template111.groovy
@@ -0,0 +1,36 @@
+// Exported from:        http://Jans-MBP:5516/#/templates/Folder3a4fc1066c0a4aea8e6f70586283129b-Release3564a34cbebf41d2bdf94b086c2a1977/releasefile
+// XL Release version:   8.1.0-SNAPSHOT
+// Date created:         Tue Aug 21 14:13:40 CEST 2018
+
+xlr {
+  template('template111') {
+    folder('kljfjkl')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-20T09:00:00+0200')
+    phases {
+      phase('Phase A') {
+        color '#0099CC'
+        tasks {
+          manual('bcd') {
+            locked true
+          }
+          manual('fg') {
+            
+          }
+          manual('abc') {
+            
+          }
+          manual('f') {
+            
+          }
+          manual('fd') {
+            
+          }
+          manual('dsaf') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
