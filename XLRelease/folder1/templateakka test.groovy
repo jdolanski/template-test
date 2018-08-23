ede507cada14c6f7e3eaa85b811540d3125f7204
jdolanski <jdolanski@users.noreply.github.com>
2018-08-23 10:38:28 +0200

    fdsljklfa

--- XLRelease/folder1/templateakka test.groovy
+++ XLRelease/folder1/templateakka test.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Jans-MBP:5516/#/templates/Folder89f25375db604d69956f790eaeb97481-Release901d1a5be9b1446b83122b1e958ba923/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Thu Aug 23 10:37:05 CEST 2018
+// Date created:         Thu Aug 23 10:38:20 CEST 2018
 
 xlr {
   template('templateakka test') {
