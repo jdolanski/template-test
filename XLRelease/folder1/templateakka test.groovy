d6ee64e8a40f668af80fa3932272024a73932399
jdolanski <jdolanski@users.noreply.github.com>
2018-08-23 10:35:15 +0200

    fkjdsajfkla

--- XLRelease/folder1/templateakka test.groovy
+++ XLRelease/folder1/templateakka test.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Jans-MBP:5516/#/templates/Folder89f25375db604d69956f790eaeb97481-Release901d1a5be9b1446b83122b1e958ba923/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Thu Aug 23 09:24:52 CEST 2018
+// Date created:         Thu Aug 23 10:35:07 CEST 2018
 
 xlr {
   template('templateakka test') {
@@ -14,6 +14,37 @@ xlr {
           }
           manual('v') {
             
+          }
+          manual('attachments') {
+            attachments {
+              attachment("sql:Attachment35c4735c58b44cfb1be5e7bdd2063cd") {
+                filename "attachments/Attachment35c4735c58b44cfb1be5e7bdd2063cd-image-4.png"
+              }
+              attachment("sql:Attachmentc8e71432c6de4d8794f28222ceb22dfe") {
+                filename "attachments/Attachmentc8e71432c6de4d8794f28222ceb22dfe-image-2.png"
+              }
+              attachment("sql:Attachment4bdceb8fd75d4bbcbd06a97f59583c2a") {
+                filename "attachments/Attachment4bdceb8fd75d4bbcbd06a97f59583c2a-VirtualBox_IE11 - Win7_20_08_2018_14_43_51.png"
+              }
+              attachment("sql:Attachmentf4910c4852924fe7a621df1fc573c57e") {
+                filename "attachments/Attachmentf4910c4852924fe7a621df1fc573c57e-Algebra_Cheat_Sheet_Reduced.pdf"
+              }
+              attachment("sql:Attachment316a790396d24bdc879a6c82d7f750dd") {
+                filename "attachments/Attachment316a790396d24bdc879a6c82d7f750dd-image-3.png"
+              }
+              attachment("sql:Attachmentc2a10310c0c941638d3676e7b7fa636b") {
+                filename "attachments/Attachmentc2a10310c0c941638d3676e7b7fa636b-image-10.png"
+              }
+              attachment("sql:Attachmentdc7c8d1146b542539ef9dba3039351c2") {
+                filename "attachments/Attachmentdc7c8d1146b542539ef9dba3039351c2-image-15.png"
+              }
+              attachment("sql:Attachment354ee37306b14a89ad4295487d6824f7") {
+                filename "attachments/Attachment354ee37306b14a89ad4295487d6824f7-image-1.png"
+              }
+              attachment("sql:Attachment7ebc6afe7730442787034d4c926a5811") {
+                filename "attachments/Attachment7ebc6afe7730442787034d4c926a5811-image-14.png"
+              }
+            }
           }
         }
       }
