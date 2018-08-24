9a16740adb57b2b91594fde3340ff3ee25068aab
jdolanski <jdolanski@users.noreply.github.com>
2018-08-24 11:10:34 +0200

    fdskljaljkf

--- XLRelease/folder1/newwww.groovy
+++ XLRelease/folder1/newwww.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Jans-MBP:5516/#/templates/Folder89f25375db604d69956f790eaeb97481-Release9f090185ff4647619d2cd6e9ce68d64b/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Fri Aug 24 11:02:36 CEST 2018
+// Date created:         Fri Aug 24 11:10:20 CEST 2018
 
 xlr {
   template('newwww') {
