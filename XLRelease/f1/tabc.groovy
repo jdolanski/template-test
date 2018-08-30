2eb0ed69d4626be563ceb909beae962f5b069005
jdolanski <jdolanski@users.noreply.github.com>
2018-08-30 11:36:10 +0200

    dfkljsfkl

--- XLRelease/f1/tabc.groovy
+++ XLRelease/f1/tabc.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Jans-MBP:5516/#/templates/Folder2e223c2571cb403bafbd1da1173fbb0d-Release3ae52105ee944b6b898f67d83e3d294d/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Thu Aug 30 11:09:33 CEST 2018
+// Date created:         Thu Aug 30 11:36:09 CEST 2018
 
 xlr {
   template('tabc') {
@@ -9,9 +9,6 @@ xlr {
     phases {
       phase('New Phase') {
         tasks {
-          manual('a') {
-            locked true
-          }
           manual('b') {
             comments {
               comment {
@@ -20,6 +17,12 @@ xlr {
                 date Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2018-08-29T10:01:32+0200')
               }
             }
+          }
+          manual('a') {
+            locked true
+          }
+          manual('g') {
+            
           }
         }
       }
