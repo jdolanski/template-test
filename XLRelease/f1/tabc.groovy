3600b7750421992c12c6a93f4ad549835203f1bb
jdolanski <jdolanski@users.noreply.github.com>
2018-08-30 11:36:46 +0200

    dfsafda

--- XLRelease/f1/tabc.groovy
+++ XLRelease/f1/tabc.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Jans-MBP:5516/#/templates/Folder2e223c2571cb403bafbd1da1173fbb0d-Release3ae52105ee944b6b898f67d83e3d294d/releasefile
 // XL Release version:   8.1.0-SNAPSHOT
-// Date created:         Thu Aug 30 11:36:09 CEST 2018
+// Date created:         Thu Aug 30 11:36:45 CEST 2018
 
 xlr {
   template('tabc') {
