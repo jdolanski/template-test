997e61db9d3a0119aa539bd808e4acf3540989cc
jdolanski <jdolanski@users.noreply.github.com>
2020-02-25 15:09:33 +0100

    akjl

---
+++ XLRelease/a/afdddddddd.groovy
@@ -0,0 +1,39 @@
+// Exported from:        http://Jans-MBP:5516/#/templates/Folder894a2e2bf84e4184b6e984fa9e86dc1c-Releaseda6d2734d90e435693c32835f4f8b3c0/releasefile
+// XL Release version:   9.6.0-alpha.90
+// Date created:         Tue Feb 25 15:09:31 CET 2020
+
+xlr {
+  template('afdddddddd') {
+    folder('a')
+    variables {
+      dateVariable('date') {
+        
+      }
+    }
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2020-02-25T09:00:00+0100')
+    realFlagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+    phases {
+      phase('New Phase') {
+        tasks {
+          custom('a') {
+            scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2020-02-26T09:45:00+0100')
+            flagStatus com.xebialabs.xlrelease.domain.status.FlagStatus.ATTENTION_NEEDED
+            comments {
+              comment {
+                text 'jgerjlfsmkls]'
+                author 'admin'
+                date Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2020-02-25T14:38:05+0100')
+                creationDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2020-02-25T14:38:05+0100')
+              }
+            }
+            script {
+              type 'checkmarx.checkOsaCompliance'
+              
+            }
+          }
+        }
+      }
+    }
+    
+  }
+}
