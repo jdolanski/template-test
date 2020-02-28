37deaf8e26b1d9713ebecb5a610a74f79774b331
jdolanski <jdolanski@users.noreply.github.com>
2020-02-28 12:05:26 +0100

    klfjfkdjlssfkladf ds fds fsd fds fsd sf fs fs fdsa fsd fds sf sfd sf
    fa
    fa
    df
    sa
    dfs
    fds
    fdss
    fd
    dfs
    fsdfs
    ds
    afd

--- XLRelease/test folder/template0.groovy
+++ XLRelease/test folder/template0.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://Jans-MBP:5516/#/templates/Folderd7431866793d452789cc5b8b1a28d238-Release8e2815331a7a40ddb9fb5e0742844321/releasefile
 // XL Release version:   9.5.0-SNAPSHOT
-// Date created:         Fri Feb 28 12:01:11 CET 2020
+// Date created:         Fri Feb 28 12:05:23 CET 2020
 
 xlr {
   template('template0') {
@@ -18,7 +18,11 @@ xlr {
             
           }
           manual('b') {
-            
+            attachments {
+              attachment("sql:Attachmentece714abd1f34d95afa3ef9e63952e0a") {
+                filename "attachments/Attachmentece714abd1f34d95afa3ef9e63952e0a-8OPcvwacmF8HLf1TlilRhGvaXWX2KC-ENUhGl6WdAr8.jpg"
+              }
+            }
           }
           manual('v') {
             
