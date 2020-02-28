c6590824b90905cffc852522f3e82f3630ee6fc8
jdolanski <jdolanski@users.noreply.github.com>
2020-02-28 12:01:13 +0100

    abcabcabc

---
+++ XLRelease/test folder/template0.groovy
@@ -0,0 +1,34 @@
+// Exported from:        http://Jans-MBP:5516/#/templates/Folderd7431866793d452789cc5b8b1a28d238-Release8e2815331a7a40ddb9fb5e0742844321/releasefile
+// XL Release version:   9.5.0-SNAPSHOT
+// Date created:         Fri Feb 28 12:01:11 CET 2020
+
+xlr {
+  template('template0') {
+    folder('test folder')
+    variables {
+      stringVariable('var') {
+        
+      }
+    }
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2020-02-28T09:00:00+0100')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('a') {
+            
+          }
+          manual('b') {
+            
+          }
+          manual('v') {
+            
+          }
+          manual(']') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
