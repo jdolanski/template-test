52afc1bec9cc6bb8cb9fe776c521c6c9f507eb20
jdolanski <jan.dolanski@ag04.com>
2022-03-14 12:41:04 +0100

    lkjslkj

--- XLRelease/fi-91410004/test.groovy
+++ XLRelease/fi-91410004/test.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://pop-os:5516/#/templates/Folder8fe97cca17584737a86f12e5764be5c9-Releaseddd671210d6449aca8a5f7e369b020ca/code
 // Release version:      10.0.4
-// Date created:         Mon Mar 14 12:38:38 CET 2022
+// Date created:         Mon Mar 14 12:41:02 CET 2022
 
 xlr {
   template('test') {
@@ -8,6 +8,7 @@ xlr {
     scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2022-03-14T09:00:00+0100')
     phases {
       phase('New Phase') {
+        color '#0079BC'
         tasks {
           manual('a') {
             
