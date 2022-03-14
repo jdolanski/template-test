c60d8bba8dfc0952476402ff17c8dc87f9950e1c
Release Administrator <>
2022-03-14 11:36:53 +0100

    lkjlkjljlkjlkjljlkj

--- XLRelease/test10.0.3/test1003.groovy
+++ XLRelease/test10.0.3/test1003.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://pop-os:5516/#/templates/Folder2beca0b421344e2cb655727eaf21f6be-Release4b5a68b934d6472e8e4813633bd2287c/code
 // Release version:      10.0.3
-// Date created:         Mon Mar 14 11:31:57 CET 2022
+// Date created:         Mon Mar 14 11:36:51 CET 2022
 
 xlr {
   template('test1003') {
