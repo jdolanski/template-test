62aa828b4a56906021cedfe985664bf17c50dfc9
Release Administrator <>
2022-03-14 12:38:40 +0100

    a

---
+++ XLRelease/fi-91410004/test.groovy
@@ -0,0 +1,26 @@
+// Exported from:        http://pop-os:5516/#/templates/Folder8fe97cca17584737a86f12e5764be5c9-Releaseddd671210d6449aca8a5f7e369b020ca/code
+// Release version:      10.0.4
+// Date created:         Mon Mar 14 12:38:38 CET 2022
+
+xlr {
+  template('test') {
+    folder('fi-91410004')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2022-03-14T09:00:00+0100')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('a') {
+            
+          }
+          manual('b') {
+            
+          }
+          manual('v') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
