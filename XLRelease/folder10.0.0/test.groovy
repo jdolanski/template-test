41d41e5c53133bc2bf8a6c4d658a291b329fe938
Release Administrator <>
2022-03-14 11:23:49 +0100

    a

---
+++ XLRelease/folder10.0.0/test.groovy
@@ -0,0 +1,26 @@
+// Exported from:        http://pop-os:5516/#/templates/Folderee8c2eb5274a4f27b95883a8fbf398d9-Releasef48c5108928f4d9680e6b384668a3e4e/code
+// Release version:      10.0.0
+// Date created:         Mon Mar 14 11:23:47 CET 2022
+
+xlr {
+  template('test') {
+    folder('folder10.0.0')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2022-03-14T09:00:00+0100')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('a') {
+            
+          }
+          manual('b') {
+            
+          }
+          manual('c') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
