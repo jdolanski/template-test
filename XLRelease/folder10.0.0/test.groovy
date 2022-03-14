8bab7112f1447828cf310007bf60bff8957f7d14
Release Administrator <jan.dolanski@ag04.com>
2022-03-14 12:23:23 +0100

    a

--- XLRelease/folder10.0.0/test.groovy
+++ XLRelease/folder10.0.0/test.groovy
@@ -1,6 +1,6 @@
 // Exported from:        http://pop-os:5516/#/templates/Folderee8c2eb5274a4f27b95883a8fbf398d9-Releasef48c5108928f4d9680e6b384668a3e4e/code
 // Release version:      10.0.0
-// Date created:         Mon Mar 14 12:21:57 CET 2022
+// Date created:         Mon Mar 14 12:23:21 CET 2022
 
 xlr {
   template('test') {
